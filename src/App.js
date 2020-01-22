022fc7bcc30260b8bc55dbcb644e556192ed9b30
moliver4 <tifftang22@gmail.com>
2020-01-22 09:59:01 -0800

    add and delete app functions complete

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends React.Component {
       email_address: this.state.email
     }
     let userPromise = Adapter.fetchUser(body)
-    
+
     userPromise.then(data => this.updateState(data))
 
     this.toggleLogin()
@@ -79,31 +79,157 @@ class App extends React.Component {
     })
   }
 
-  addUserInfoHandler =(obj) => {
-    console.log('add user info')
+  addObjFetchHandler = (name, obj) => {
+    switch(name){
+      case 'skill':
+        let temp = Adapter.addSkill(obj)
+        temp.then(data => this.addObjStateHandler('skill', data))
+        break
+      case 'project':
+        let temp = Adapter.addProject(obj)
+        temp.then(data => this.addObjStateHandler('project', data))
+        break
+      case 'education':
+        let temp = Adapter.addEducation(obj)
+        temp.then(data => this.addObjStateHandler('education', data))
+        break
+      case 'experience':
+        let temp = Adapter.addExperience(obj)
+        temp.then(data => this.addObjStateHandler('experience', data))
+        break
+      case 'accolade':
+        let temp = Adapter.addAccolade(obj)
+        temp.then(data => this.addObjStateHandler('accolade', data))
+        break
+      default:
+        console.log('i got nothing, nothing was added and i cry')
+        break
+    }
   }
 
-  addSkillHandler =(obj) => {
-    console.log('add skill')
+  addObjStateHandler = (name, data) => {
+    switch(name){
+      case 'skill':
+        this.setState(prevState=> {
+          return {
+            skills: [...prevState.skills, data]
+          }
+        }, () => console.log('skill added') )
+        break
+      case 'project':
+        this.setState(prevState=> {
+          return {
+            projects: [...prevState.projects, data]
+          }
+        }, () => console.log('project added') )
+        break
+      case 'education':
+        this.setState(prevState=> {
+          return {
+            education: [...prevState.education, data]
+          }
+        }, () => console.log('education added') )
+        break
+      case 'experience':
+        this.setState(prevState=> {
+          return {
+            experiences: [...prevState.experiences, data]
+          }
+        }, () => console.log('experience added') )
+        break
+      case 'accolade':
+        this.setState(prevState=> {
+          return {
+            accolades: [...prevState.accolades, data]
+          }
+        }, () => console.log('accolade added') )
+        break
+      default:
+        console.log('nothing was added and everything is broken')
+        break
+    }
   }
 
-  addProjectHandler =(obj) => {
-    console.log('add Project')
+
+  deleteObjFetchHandler = (name, id) => {
+    switch(name){
+      case 'skill':
+        let temp = Adapter.deleteSkill(id)
+        temp.then(data => this.deleteObjStateHandler('skill', data))
+        break
+      case 'project':
+        let temp = Adapter.deleteProject(id)
+        temp.then(data => this.deleteObjStateHandler('project', data))
+        break
+      case 'education':
+        let temp = Adapter.deleteEducation(id)
+        temp.then(data => this.deleteObjStateHandler('education', data))
+        break
+      case 'experience':
+        let temp = Adapter.deleteExperience(id)
+        temp.then(data => this.deleteObjStateHandler('experience', data))
+        break
+      case 'accolade':
+        let temp = Adapter.deleteAccolade(id)
+        temp.then(data => this.deleteObjStateHandler('accolade', data))
+        break
+      default:
+        console.log('i got nothing, delete fetch broken')
+        break
+    }
   }
 
-  addEducationHandler =(obj) => {
-    console.log('add Education')
+  deleteObjStateHandler = (name, data) => {
+    console.log(`${name} data: ${data} made it to set state handler for delete`)
+    switch(name){
+      case 'skill':
+        this.setState(prevState=> {
+          return {
+            skill: this.deleteHelper(prevState.skills, data.id)
+          }
+        }, () => console.log('skill deleted') )
+        break
+      case 'project':
+        this.setState(prevState=> {
+          return {
+            project: this.deleteHelper(prevState.projects, data.id)
+          }
+        }, () => console.log('project deleted') )
+        break
+      case 'education':
+        this.setState(prevState=> {
+          return {
+            education: this.deleteHelper(prevState.education, data.id)
+          }
+        }, () => console.log('education deleted') )
+        break
+      case 'experience':
+        this.setState(prevState=> {
+          return {
+            experience: this.deleteHelper(prevState.experiences, data.id)
+          }
+        }, () => console.log('experience deleted') )
+        break
+      case 'accolade':
+        this.setState(prevState=> {
+          return {
+            accolade: this.deleteHelper(prevState.accolades, data.id)
+          }
+        }, () => console.log('accolade deleted') )
+        break
+      default:
+        console.log('nothing was deleted and everything is broken')
+        break
+    }
   }
 
-  addExperienceHandler =(obj) => {
-    console.log('add Experience')
-  }
-
-  addAccoladeHandler =(obj) => {
-    console.log('add Accolade')
+  deleteHelper = (prev, id) => {
+    console.log(prev, id)
+    return prev.filter(o => o.id !== id)
   }
 
 
+  
   render() {
     return (
       <div>
--- src/services/Adapter.js
+++ src/services/Adapter.js
@@ -1,12 +1,18 @@
 const USERSURL = 'http://localhost:3000/users'
-// const SKILLSURL = 'http://localhost:3000/skills'
-// const PROJECTSURL = 'http://localhost:3000/projects'
-// const EDUCATIONURL = 'http://localhost:3000/education'
-// const EXPERIENCESURL = 'http://localhost:3000/experiences'
-// const ACCOLADESURL = 'http://localhost:3000/accolades'
+const SKILLSURL = 'http://localhost:3000/skills'
+const PROJECTSURL = 'http://localhost:3000/projects'
+const EDUCATIONURL = 'http://localhost:3000/education'
+const EXPERIENCESURL = 'http://localhost:3000/experiences'
+const ACCOLADESURL = 'http://localhost:3000/accolades'
+//to use this class, import into the page as Adapter.
+//have an object assembled with necessary information to pass into the method. some methods will need the ID as well
+//call methods as Adapter.fetchUser(obj) this will return a promise that you will do something with
+//make sure to assign the promise to a variable so you can do a .then(data=> method(data))
 
  class Adapter {
 
+//add or find user via POST request
+//will return all of the users data for other fields as well 
   static fetchUser(body){
     return fetch(`${USERSURL}`, {
             method: 'POST',
@@ -19,19 +25,7 @@ const USERSURL = 'http://localhost:3000/users'
             .then(res => res.json())
   }
 
-
-  static fetchUser(body){
-    return fetch(`${USERSURL}`, {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-            },
-            body: JSON.stringify(body)
-            })
-            .then(res => res.json())
-  }
-
+  //adding field
   static addSkill(body){
     return fetch(`${USERSURL}`, {
             method: 'POST',
@@ -92,29 +86,8 @@ const USERSURL = 'http://localhost:3000/users'
             .then(res => res.json())
   }
 
-  static editUser(id, body){
-    return fetch(`${USERSURL}/${id}`, {
-            method: 'PATCH',
-            headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-            },
-            body: JSON.stringify(body)
-            })
-            .then(res => res.json())
-  }
 
-  static editUser(id, body){
-    return fetch(`${USERSURL}/${id}`, {
-            method: 'PATCH',
-            headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-            },
-            body: JSON.stringify(body)
-            })
-            .then(res => res.json())
-  }
+  //edits
 
   static editUser(id, body){
     return fetch(`${USERSURL}/${id}`, {
@@ -187,6 +160,73 @@ const USERSURL = 'http://localhost:3000/users'
             })
             .then(res => res.json())
   }
+
+  //deletes
+
+  static deleteUser(id){
+    return fetch(`${USERSURL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'}
+            })
+            .then(res => res.json())
+  }
+
+  static deleteSkill(id){
+    return fetch(`${SKILLSURL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'}
+            })
+            .then(res => res.json())
+  }
+  
+  static deleteProject(id){
+    return fetch(`${PROJECTSURL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'}
+            })
+            .then(res => res.json())
+  }
+
+  static deleteEducation(id){
+    return fetch(`${EDUCATIONURL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'}
+            })
+            .then(res => res.json())
+  }
+
+  static deleteExperience(id){
+    return fetch(`${EXPERIENCESURL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'}
+            })
+            .then(res => res.json())
+  }
+
+
+
+
+  static deleteAccolade(id){
+    return fetch(`${ACCOLADESURL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'}
+            })
+            .then(res => res.json())
+  }
+
+
 
 
 
