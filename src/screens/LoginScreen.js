81835689741aede7a9ed942c3d88ef95e08861df
learn-co-kat <twothistles@gmail.com>
2020-01-23 14:31:50 -0800

    Items centered.

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,27 +4,29 @@ import logo from '../images/logo1.png'
 
 // add form here that will take in user email & submit button
 // submit button will trigger a function in App.js that will retrieve all user details
+const styles = {
+    backgroundColor: '#hgfhg'
+}
 const LoginScreen = ({ email, submitLogin, handleInputChange}) => {
 
     return (
-        <div class="container">
-            <img src={logo} ></img>
+        <div className="container text-center" >
 
-            <div class=".flex-row">
-                <h4>Login or SignUp to Get Started</h4>
-            </div>
-
-            <div class=".flex-row">
-                <form id="login-form" onSubmit={submitLogin}>
-                            <label htmlFor='email_address'> </label> 
-                            <input placeholder="Enter email"
-                                type='text' 
-                                value={email} 
-                                name='email_address'
-                                onChange={(e) => handleInputChange(e)}
-                            /> <br></br>
-                    <input type='submit' value='Login' />
-                </form>
+            <div className='row py-5'>
+                <div className='col '>
+                    <img className='py-5' width="150px" src={logo} ></img>
+                    <h4>Login or SignUp to Get Started</h4>
+                    <form className='py-3' id="login-form" onSubmit={submitLogin}>
+                                <label htmlFor='email_address'> </label> 
+                                <input placeholder="Enter email"
+                                    type='text' 
+                                    value={email} 
+                                    name='email_address'
+                                    onChange={(e) => handleInputChange(e)}
+                                /> <br></br>
+                        <input type='submit' value='Login' />
+                    </form>
+                </div>
             </div>
             
         </div>
