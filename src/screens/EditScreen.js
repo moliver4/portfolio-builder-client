84fad3cd54a7c55c9414709e8fa11b16d3b9d17e
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-22 16:59:43 -0800

    updated edit screen. Now navigates to portfolio page

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -3,14 +3,17 @@ import EditProjectsContainer from '../containers/EditProjectsContainer.js'
 import EditEducationContainer from '../containers/EditEducationContainer.js'
 import LoginHOC from '../HOCs/LoginHOC'
 import User from '../containers/EditUserContainer'
-
 import EditExperienceContainer from '../containers/EditExperienceContainer'
+import { Link } from 'react-router-dom'
 
 class EditScreen extends Component {
 
-// render containers for each field to be edited 
 
 // 2 buttons at bottom!!! 1 will be button to SAVE the data(although we are already saving it) and other must be a 'Link' to '/portoflio/:id'
+// Do we still need two buttons? Components look like they are saving data independently so all should be updated?
+
+    portfolioLink = `/portfolio/${this.props.user.id}`
+
     render() {
         {console.log("these are the props in Edit Screen", this.props)}
         return (
@@ -18,10 +21,14 @@ class EditScreen extends Component {
                 <User user={this.props.user} editUserInfo={this.props.editObj}/>
                 <EditProjectsContainer userInfo={this.props.user} addObj={this.props.addObj} deleteObj={this.props.deleteObj} editObj={this.props.editObj} projects={this.props.projects} ></EditProjectsContainer>
                 <EditEducationContainer userInfo={this.props.user} addObj={this.props.addObj} deleteObj={this.props.deleteObj} editObj={this.props.editObj} education={this.props.education} ></EditEducationContainer>
-                {/* submit button */}
-                {/* portfolio button */}
-                <User user={this.props.user} addUserInfo={this.props.addUserInfo}/>
                 <EditExperienceContainer experiences={this.props.experiences} user={this.props.user} addObj={this.props.addObj} editObj={this.props.editObj} deleteObj={this.props.deleteObj} />
+                <div>
+                    <br/>
+                    <Link to={this.portfolioLink}>
+                    <button><h4>Build Portfolio!</h4></button>
+                    </Link>
+                    <br/>
+                </div>
             </div>
         )
     }
