4c96cdb1bc7e4b4f99cc1a2362c0e5ddf6b015c3
learn-co-kat <twothistles@gmail.com>
2020-01-23 15:06:21 -0800

    CSS continues. Video, welcome note present.

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,7 +5,7 @@ import logo from '../images/logo1.png'
 // add form here that will take in user email & submit button
 // submit button will trigger a function in App.js that will retrieve all user details
 const styles = {
-    backgroundColor: '#hgfhg'
+    backgroundColor: '#0fabbc'
 }
 const LoginScreen = ({ email, submitLogin, handleInputChange}) => {
 
@@ -26,10 +26,23 @@ const LoginScreen = ({ email, submitLogin, handleInputChange}) => {
                                     name='email_address'
                                     onChange={(e) => handleInputChange(e)}
                                 /> <br></br>
-                        <input type='submit' value='Login' />
+                        <input type='button' class="btn btn-primary" value='Login' />
                     </form>
-                    <h2>Welcome to the MVP porfolio builder!</h2>
-                    <h4>Where you can create your own portfolio and share it with the world.</h4>
+
+                    <div class="p-3 mb-2 bg-info text-white">
+                        <h2>Welcome to the MVP porfolio builder!</h2>
+                        <h4>Where you can create your own portfolio and share it with the world.</h4>
+                    </div>
+
+                    <div>
+                        <iframe src='https://www.youtube.com/embed/EcEMX-63PKY'
+                        frameBorder='0'
+                        allow='autoplay;'
+                        allowFullScreen
+                        title='video'
+                        width="723" height="542"
+                        />
+                    </div>
                     
                 </div>
 
