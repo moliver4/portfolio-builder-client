38858ebbe5e9c26dbe7248eecda78b092c3ca0a4
moliver4 <tifftang22@gmail.com>
2020-01-23 12:10:26 -0800

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
-const NavBar 
+const NavBar = () => {
+    return (
+        <div className='navbar' role="navigation">
+            hello there i am a navbar
+
+        </div>
+    )
+}
 
 
 export default NavBar
