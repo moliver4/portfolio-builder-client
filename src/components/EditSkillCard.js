123722ce8425a3036aefd55b00d6c6f20b6ff801
github username <github email address>
2020-01-23 17:57:54 -0800

    edit css

--- src/components/EditSkillCard.js
+++ src/components/EditSkillCard.js
@@ -8,19 +8,6 @@ export default class EditSkillCard extends Component {
 
     render() {
         return (
-            <div>
-            <p><strong>Name</strong> {this.props.cardDetails.name}</p> 
-            <p><strong>Category</strong> {this.props.cardDetails.category}</p> 
-            <p><strong>External Link</strong> {this.props.cardDetails.link_url}</p> 
-            <p><strong>Proficiency</strong> {this.props.cardDetails.proficiency}</p> 
-            <p><strong>Display Order</strong> {this.props.cardDetails.order}</p> 
-            <button onClick={e => this.props.changeEditing(e, this.props.cardDetails)} type="button">Edit</button>
-            <button onClick={e => this.props.deleteObj('skill', this.props.cardDetails.id)} type="button">Delete</button>
-            <br/>            
-            <br/>  
-
-
-
             <div className="card">
                     <div className="card-header">
                     <ul className="nav nav-pills card-header-pills">
@@ -40,7 +27,6 @@ export default class EditSkillCard extends Component {
                         <p class="card-text">Priority : {this.props.cardDetails.order}</p>
                     </div>
                 </div>          
-        </div>
         )
     }
 }
