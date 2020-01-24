58c6ac18d7aff0a00e92e0a08b1a4087e8813e92
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-23 16:56:53 -0800

    making progress on bio for portfolio

--- src/containers/PortfolioUserContainer.js
+++ src/containers/PortfolioUserContainer.js
@@ -5,12 +5,38 @@ export class PortfolioUserContainer extends Component {
 
     
     render() {
+        const {id, name, email_address, avatar_url, background_url, template} = this.props.user
         return (
-            <div>
-               {this.props.user.id}
+            <div className='card'>
+                {/* <div className='row'> */}
+                <img className='card-img' src={background_url} alt='Card Image'/>
+                <div className='card-body'>
+                        <h3>{name}</h3>
+                        <a href={email_address} alt='email address'>{email_address}</a>
+                </div>
+
+                    <div className='card-img-overlay position-relative'>
+                        <img src={avatar_url} className='mx-auto d-block position-relative' width='300px' style={style.avatar}/>
+                    </div>                 
+                {/* </div> */}
             </div>
         )
     }
 }
+
+const style = {
+    avatar: {
+        maxHeight: '300px',
+        borderRadius: 500,
+        maxWidth: '300px'
+    },
+    avatar_div: {
+        // paddingTop: '28%',
+        paddingBottom: '50px',
+
+    }
+}
+
+
 
 export default PortfolioUserContainer
