d0c82cccbef0cd54dc572e42f6978563aefd7c5c
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-24 09:20:04 -0800

    udpated bio for portfolio

--- src/containers/PortfolioUserContainer.js
+++ src/containers/PortfolioUserContainer.js
@@ -5,14 +5,20 @@ export class PortfolioUserContainer extends Component {
 
     
     render() {
-        const {id, name, email_address, avatar_url, background_url, template} = this.props.user
+        const {name, email_address, avatar_url, background_url, template} = this.props.user
         return (
-            <div className='card'>
+            <div className='card' style={style.bio_card}>
                 {/* <div className='row'>/ */}
                 <img className='card-img' src={background_url} alt='Card Image' style={style.background_img}/>
-                <div className='card-body'>
-                        <h3 className='align-self-center'>{name}</h3>
-                        <a href={email_address} alt='email address'>{email_address}</a>
+                <div className='card-body row justify-content-center'>
+                        {/* <p><a href={email_address} alt='email address'>{email_address}</a></p> */}
+                </div>
+                <div className='card-body row justify-content-center'>
+                        <h3>{name}</h3>
+                </div>
+                <div className='card-body row justify-content-center'>
+                        <a href={email_address} alt='email address'>Email Me</a>
+                        <p></p>
                 </div>
                     <div className='card-img-overlay position-relatives align-self-center' style={style.avatar_div}>
                         <img src={avatar_url} className='mx-auto d-block position-inherit align-self-center' width='350px' style={style.avatar}/>
@@ -35,6 +41,9 @@ const style = {
     },
     background_img: {
         maxHeight: '400px'
+    },
+    bio_card: {
+        marginBottom: '50px'
     }
 }
 
