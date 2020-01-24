5003ab28a9f5bada2b380161273235fe5d75cd5e
learn-co-kat <twothistles@gmail.com>
2020-01-24 11:13:33 -0800

    Items align in each column but width not 100%.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,39 @@
 import logo from '../images/logo2.png'
+import dinosoftlabs from '../images/dinosoftlabs.jpg'
 import React from 'react'
 
 // Created by: names... (links to github )
 // front-end & back repo links
 // credit to logo peeps
+// <img className="d-inline-block align-left" src={logo} style={styles.footerLogo} ></img>
 
 function Footer() {
     return (
         <footer style={styles.footer}>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm">
-                        <img src={logo} style={styles.footerLogo} ></img>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm">
+                        <h6 style={styles.projectRepo} style={styles.textcolor}>Download Portfolio Builer Lite</h6>
+                        <h5 className="btn btn-outline-light">front-end repo</h5>
+                        <h5 className="btn btn-outline-light">back-end repo</h5>
                     </div>
-                    <div class="col-sm">
-                        <h6 style={styles.projectRepo}>Download our front-end / back-end repository</h6>
-                            <div>
-                                <p>Created by:</p>
-                                <a href=''>Tiffany Tang</a>
-                                <a href=''>Devin Kelly</a>
-                                <a href=''>Taylor Coon</a>
-                                <a href=''>Katrina Reinsdorff</a>
-                            </div>
+                    <div className="col-sm">
+                        <p style={styles.textcolor}>Created by</p>
+                                <a className="btn btn-outline-light" style={styles.textcolor} href=''>Tiffany Tang</a>
+                                <a className="btn btn-outline-light" style={styles.textcolor} href=''>Devin Kelly</a>
+                                <a className="btn btn-outline-light" style={styles.textcolor} href=''>Taylor Coon</a>
+                                <a className="btn btn-outline-light" style={styles.textcolor} href=''>Katrina Reinsdorff</a>
                     </div>
-                    <div class="col-sm">
-                        <a href='https://www.flaticon.com/authors/dinosoftlabs'>Logo credit: DinosoftLabs</a>
+                    <div className="col-sm">
+                        <p style={styles.textcolor}>Logo credit</p>
+                        <img src={dinosoftlabs} style={styles.dino} href='https://www.flaticon.com/authors/dinosoftlabs'></img>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col">
+                    </div>
                 </div>
-
-            
-
-
-            
-
+            </div>
         </footer>
     )
 }
@@ -42,10 +42,11 @@ export default Footer
 
 const styles = {
     footer: {
-        backgroundColor: '#12cad6',
-        height: 110,
-        width: '100%',
-        marginTop: 60
+        backgroundColor: '#111111',
+        height: 'auto',
+        width: 'auto',
+        marginTop: 60,
+        padding: 20
     },
     footerLogo: {
         width: 70,
@@ -56,5 +57,12 @@ const styles = {
     },
     projectRepo: {
         text_align: 'center'
+    },
+    textcolor: {
+        color: '#ffffff'
+    },
+    dino: {
+        borderRadius: 15,
+        width: 55
     }
 }
