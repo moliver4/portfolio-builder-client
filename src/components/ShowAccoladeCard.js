b6c7fbed9f67c665b38134a7b6c24eb08c7ee52c
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-24 11:33:07 -0800

    formatted Accolades to fit in with other cards on Portfolio screen

--- src/components/ShowAccoladeCard.js
+++ src/components/ShowAccoladeCard.js
@@ -12,8 +12,7 @@ const ShowAccoladeCard = (props) => {
           <div className="card text-left">
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{summary}</h6>
-                <h7 className="card-subtitle mb-2 text-muted">Graduation date</h7>
+                <h6 className="card-text mb-2 text-muted">{summary}</h6>
                 <a href={link} className="card-link">Look</a>
             </div>
         </div>
