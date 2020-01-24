da1fe0019718f8e5731ac4f914812611293cfdd9
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-24 11:51:29 -0800

    added dynamic links to skills card

--- src/components/ShowSkillsCard.js
+++ src/components/ShowSkillsCard.js
@@ -27,7 +27,11 @@ const ShowSkillsCard = (props) => {
                 <h5 className="card-title">{name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{category}</h6>
                 <p className="card-text">{showProf(proficiency)}</p>
+                {link_url ?
                 <a href={link_url} className="card-link">Look</a>
+                : 
+                <a></a>
+                }
             </div>
         </div>
   
