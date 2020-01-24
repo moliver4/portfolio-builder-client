e6224520e1c9065c42c9c4571a1210514b527140
github username <github email address>
2020-01-24 09:01:06 -0800

    edit screen css

--- src/components/EditExperienceCard.js
+++ src/components/EditExperienceCard.js
@@ -10,22 +10,26 @@ const { company, role, summary, start_date, end_date, order } = props.experience
 
 // replace class names
   return (
-    <div className="ui card"> 
-        <div className="Experience">
-            <div>
-                <p><strong>Company</strong> {company}</p>
-                <p><strong>Role</strong> {role}</p>
-                <p><strong>Summary</strong> {summary}</p>
-                <p><strong>Start Date</strong> {start_date}</p>
-                <p><strong>End Date</strong> {end_date}</p>
-                <p><strong>Order</strong> {order}</p>
-                <button onClick={() => props.editExistingExperience(props.experience)}>Edit</button>
-                <button onClick={() => props.deleteObj('experience', props.experience.id)} >Delete</button>
-                <br/>
-                <br/>
-            </div>
-        </div>
-    </div>
+<div className="card">
+<div className="card-header">
+<ul className="nav nav-pills card-header-pills">
+<li className="nav-item">
+    <button onClick={() => props.editExistingExperience(props.experience)} class="nav-link" >Edit</button>
+</li>
+<li className="nav-item">
+    <button onClick={() => props.deleteObj('experience', props.experience.id)} className="nav-link" >Delete</button>
+</li>
+</ul>
+</div>
+<div class="card-body">
+    <h5 class="card-title">Company : {company}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Role : {role}</h6>
+    <p class="card-text">Summary : {summary}</p>
+    <p class="card-text">Start Date : {start_date}</p>
+    <p class="card-text">End Date : {end_date}</p>
+    <p class="card-text">priority : {order}</p>
+</div>
+</div>
   )
 };
 
